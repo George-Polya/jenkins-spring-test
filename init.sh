65c050afb5b4c6f753528be457a4496992b8b529
김희수 <wlsakak4@gmail.com>
2024-01-21 09:55:26 +0000

    fix:spring/init.sh에 네트워크 생성 추가

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo docker build --no-cache -t spring-image .
 
+sudo docker network create -d bridge test-bridge
+
 sudo docker run -p 7000:80 -d --name spring-container --net test-bridge spring-image
