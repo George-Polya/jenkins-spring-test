bd87a3cfc4bcbd6515d8ba4f401d02835e3515cf
김희수 <wlsakak4@gmail.com>
2024-01-21 09:52:48 +0000

    fix: spring/init.sh에 sudo 명령어 추가

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-docker build --no-cache -t spring-image .
+sudo docker build --no-cache -t spring-image .
 
-docker run -p 7000:80 -d --name spring-container --net test-bridge spring-image
+sudo docker run -p 7000:80 -d --name spring-container --net test-bridge spring-image
